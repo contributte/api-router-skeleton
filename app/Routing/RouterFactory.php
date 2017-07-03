469a5a56d61e9ec6cf3991f0538ff85f630075e2
FXik <216508546+fxik@users.noreply.github.com>
2017-07-03 10:19:18 +0200

    Added example codes

---
+++ app/Routing/RouterFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Routing;
+
+use Nette\Application\Routers\RouteList;
+
+final class RouterFactory
+{
+
+	public function create(): RouteList
+	{
+		$router = new RouteList;
+
+		return $router;
+	}
+
+}
