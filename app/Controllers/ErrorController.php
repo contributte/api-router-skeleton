cb077a0fcd52ccbdf08be90e9bd1ae1074abb960
FXik <216508546+fxik@users.noreply.github.com>
2021-10-06 21:45:30 +0200

    Logging html of exception

--- app/Controllers/ErrorController.php
+++ app/Controllers/ErrorController.php
@@ -7,12 +7,17 @@ namespace App\Controllers;
 use Nette\Application\IResponse;
 use Nette\Application\Request;
 use Nette\Application\Responses\JsonResponse;
+use Tracy\Debugger;
+use Tracy\ILogger;
 
 final class ErrorController extends AbstractController
 {
 	public function run(Request $request): IResponse
 	{
 		$exception = $request->getParameter('exception');
+		if ($exception instanceof \Throwable) {
+			Debugger::log($exception, ILogger::EXCEPTION);
+		}
 
 		return new JsonResponse($this->apiResponseFormatter->formatException($exception));
 	}
